bd85f1b988849b22960ae228e5e299590a7a74e5
ChrisH0125 <ho.chris.cs@gmail.com>
2025-09-27 10:29:28 -0400

    Customized the navbar

--- frontend/components/Logo.tsx
+++ frontend/components/Logo.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 const Logo = () => {
     return (
-        <div className="logo h-20 w-20">
-            <img src="/memorai2.png" alt="logo" className="h-full w-full object-contain" />
-        </div>
+        <Link href="/" className="logo h-20 w-20 block hover:scale-105 transition-transform duration-300 cursor-pointer">
+            <img src="/memorai2.png" alt="MemorAI logo" className="h-full w-full object-contain" />
+        </Link>
     );
 };
 
