f5fa26065d281fa79e6fb44f072eb80100eb84ae
Richard Phillips <richardphillipscs@gmail.com>
2025-09-27 01:19:15 -0400

    added upload buttons and frontend for caregiver

--- frontend/app/Caregiver/page.tsx
+++ frontend/app/Caregiver/page.tsx
@@ -1,31 +1,50 @@
 export default function Caregiver() {
   return (
-    <div className="min-h-screen bg-gray-50 flex">
-      <div className="flex-1 p-6 flex justify-end">
-        <div className="w-1/2 grid grid-cols-1 gap-2 h-min-screen">
-
-          <div className="bg-white rounded-lg shadow p-9">
-            <h2 className="text-xl font-semibold text-gray-900 mb-4"></h2>
-            <div className="h-30 bg-gray-50 rounded border-2 border-dashed border-gray-300 flex items-center justify-center">
-              <p className="text-gray-500">UPLOAD</p>
-            </div>
+    <div className="min-h-screen bg-gray-50 p-6 pt-20">
+      <div className="max-w-7xl mx-auto mt-3">
+        {/*First row*/}
+        <div className="flex justify-center items-center gap-12 mb-6">
+          <div className="w-80 bg-white rounded-lg shadow p-4 flex items-center justify-center h-20">
+            <h2 className="text-lg font-semibold text-gray-900">Name</h2>
           </div>
 
-
-          <div className="bg-white rounded-lg shadow p-6">
-            <h2 className="text-xl font-semibold text-gray-900 mb-4"></h2>
-            <div className="h-30 bg-gray-50 rounded border-2 border-dashed border-gray-300 flex items-center justify-center">
-              <p className="text-gray-500">UPLOAD</p>
+          <div className="w-80 bg-white rounded-lg shadow p-4">
+            <h2 className="text-lg font-semibold text-gray-900 mb-3"></h2>
+            <div className="h-32 bg-gray-50 rounded border-2 border-dashed border-gray-300 flex items-center justify-center">
+              <p className="text-sm text-gray-500">UPLOAD</p>
             </div>
           </div>
+        </div>
 
-          <div className="bg-white rounded-lg shadow p-6">
-            <h2 className="text-xl font-semibold text-gray-900 mb-4"></h2>
-            <div className="h-30 bg-gray-50 rounded border-2 border-dashed border-gray-300 flex items-center justify-center">
-              <p className="text-gray-500">UPLOAD</p>
+        {/*Second row*/}
+        <div className="flex justify-center items-center gap-12 mb-6">
+          <div className="w-80 bg-white rounded-lg shadow p-4 flex items-center justify-center h-20">
+            <h2 className="text-lg font-semibold text-gray-900">
+              Relationship
+            </h2>
+          </div>
+
+          <div className="w-80 bg-white rounded-lg shadow p-4">
+            <h2 className="text-lg font-semibold text-gray-900 mb-3"></h2>
+            <div className="h-32 bg-gray-50 rounded border-2 border-dashed border-gray-300 flex items-center justify-center">
+              <p className="text-sm text-gray-500">UPLOAD</p>
             </div>
           </div>
-        </div>    
+        </div>
+
+        {/*Third row*/}
+        <div className="flex justify-center items-center gap-12">
+          <div className="w-80 bg-white rounded-lg shadow p-4 flex items-center justify-center h-20">
+            <h2 className="text-lg font-semibold text-gray-900">Activity</h2>
+          </div>
+
+          <div className="w-80 bg-white rounded-lg shadow p-4">
+            <h2 className="text-lg font-semibold text-gray-900 mb-3"></h2>
+            <div className="h-32 bg-gray-50 rounded border-2 border-dashed border-gray-300 flex items-center justify-center">
+              <p className="text-sm text-gray-500">UPLOAD</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
